4827384637defd179ae8a26d9ddbc91f85ba81ae
yarivkraus <yariv.kraus@gmail.com>
2026-01-20 15:24:07 +0200

    Update styling.js

--- styling.js
+++ styling.js
@@ -1,7 +1,8 @@
 (function () {
-  console.log("SAC Widget Styling - Script Execution Started (v1.0.3)");
-  let template = document.createElement("template");
-  template.innerHTML = `
+  try {
+    console.log("SAC Widget Styling - Script Execution Started (v1.0.5)");
+    let template = document.createElement("template");
+    template.innerHTML = `
     <style>
       /* Styling panel for SAC custom widget */
       .styling-panel {
@@ -51,7 +52,7 @@
     </style>
     
     <div class="styling-panel">
-      <div style="font-size: 10px; color: #999; margin-bottom: 10px;">Styling Panel v1.0.3</div>
+      <div style="font-size: 10px; color: #999; margin-bottom: 10px;">Styling Panel v1.0.5</div>
       <div class="styling-section">
         <label class="styling-label" for="titleInput">Widget Title</label>
         <input type="text" class="styling-input" id="titleInput" placeholder="Enter widget title">
@@ -77,56 +78,59 @@
     </div>
   `;
 
-  class TableWidgetStyling extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    class TableWidgetStyling extends HTMLElement {
+      constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-      this._initializeElements();
-      this._attachEventListeners();
-    }
+        this._initializeElements();
+        this._attachEventListeners();
+      }
 
-    _initializeElements() {
-      this._titleInput = this.shadowRoot.getElementById("titleInput");
-      this._showInputCheckbox = this.shadowRoot.getElementById("showInputCheckbox");
-      this._maxRowsInput = this.shadowRoot.getElementById("maxRowsInput");
-      this._primaryColorPicker = this.shadowRoot.getElementById("primaryColorPicker");
-    }
+      _initializeElements() {
+        this._titleInput = this.shadowRoot.getElementById("titleInput");
+        this._showInputCheckbox = this.shadowRoot.getElementById("showInputCheckbox");
+        this._maxRowsInput = this.shadowRoot.getElementById("maxRowsInput");
+        this._primaryColorPicker = this.shadowRoot.getElementById("primaryColorPicker");
+      }
 
-    _attachEventListeners() {
-      this._titleInput.addEventListener("input", () => {
-        this._firePropertyChange("title", this._titleInput.value);
-      });
+      _attachEventListeners() {
+        this._titleInput.addEventListener("input", () => {
+          this._firePropertyChange("title", this._titleInput.value);
+        });
 
-      this._showInputCheckbox.addEventListener("change", () => {
-        this._firePropertyChange("showInputFields", this._showInputCheckbox.checked);
-      });
+        this._showInputCheckbox.addEventListener("change", () => {
+          this._firePropertyChange("showInputFields", this._showInputCheckbox.checked);
+        });
 
-      this._maxRowsInput.addEventListener("input", () => {
-        this._firePropertyChange("maxRows", parseInt(this._maxRowsInput.value));
-      });
+        this._maxRowsInput.addEventListener("input", () => {
+          this._firePropertyChange("maxRows", parseInt(this._maxRowsInput.value));
+        });
 
-      this._primaryColorPicker.addEventListener("input", () => {
-        this._firePropertyChange("primaryColor", this._primaryColorPicker.value);
-      });
-    }
+        this._primaryColorPicker.addEventListener("input", () => {
+          this._firePropertyChange("primaryColor", this._primaryColorPicker.value);
+        });
+      }
 
-    _firePropertyChange(propertyName, value) {
-      this.dispatchEvent(new CustomEvent("propertiesChanged", {
-        detail: {
-          properties: {
-            [propertyName]: value
+      _firePropertyChange(propertyName, value) {
+        this.dispatchEvent(new CustomEvent("propertiesChanged", {
+          detail: {
+            properties: {
+              [propertyName]: value
+            }
           }
-        }
-      }));
+        }));
+      }
     }
-  }
-  console.log("SAC Widget Styling - Script loaded");
-  try {
-    customElements.define("com-yarivkraus-tablewidget-styling", TableWidgetStyling);
-    console.log("SAC Widget Styling - Custom Element defined");
+    console.log("SAC Widget Styling - Script loaded");
+    try {
+      customElements.define("com-yarivkraus-tablewidget-styling", TableWidgetStyling);
+      console.log("SAC Widget Styling - Custom Element defined");
+    } catch (e) {
+      console.warn("SAC Widget Styling - Custom Element might already be defined:", e);
+    }
   } catch (e) {
-    console.warn("SAC Widget Styling - Custom Element might already be defined:", e);
+    console.error("SAC Widget Styling - TOP LEVEL CRASH in styling.js:", e);
   }
 })();
